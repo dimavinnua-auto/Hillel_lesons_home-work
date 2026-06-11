fee2c29e23076c550875cde461e5fd0e91ffe60d
dimavinnua-auto <dima.vinn.ua@gmail.com>
2026-06-11 16:31:02 +0300

    Add homeWork8.py

--- HomeWork8/HomeWork8.py
+++ HomeWork8/HomeWork8.py
@@ -2,15 +2,15 @@
 # Створіть клас "Студент" з атрибутами "ім'я", "прізвище", "вік" та "середній бал". Створіть об('єкт цього класу, '
 # 'представляючи студента. Потім додайте метод до класу "Студент", ''який дозволяє змінювати середній бал студента.'
 # "Виведіть інформацію про студента та змініть його середній бал)
-class students():
+class Students():
     def __init__(self,name, secondname, age, grade):
         self.name = name
         self.secondname = secondname
         self.age = age
         self.grade = grade
 
-one_students = students(name= 'Ivan', secondname='Golang', age = 21, grade = 10)
-print( f"Name - {one_students.name},\nSoname - {one_students.secondname},\n"
-       f"Age -{one_students.age},\nAverageGrade - {one_students.grade}")
+one_student = Students(name= 'Ivan', secondname='Golang', age = 21, grade = 10)
+print( f"Name - {one_student.name},\nSoname - {one_student.secondname},\n"
+       f"Age -{one_student.age},\nAverageGrade - {one_student.grade}")
 
 
